e450a7108f9911b946c41b3c712c7285bd0d18cd
Ashleywairimu92 <ashleywairimu18@gamil.com>
2024-08-18 23:15:42 +0300

    updates

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,57 @@
-import React from "react";
-import CategoryFilter from "./CategoryFilter";
-import NewTaskForm from "./NewTaskForm";
-import TaskList from "./TaskList";
+// import React from "react";
+// import CategoryFilter from "./CategoryFilter";
+// import NewTaskForm from "./NewTaskForm";
+// import TaskList from "./TaskList";
 
-import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
+// import { CATEGORIES, TASKS } from "../data";
+// console.log("Here's the data you're working with");
+// console.log({ CATEGORIES, TASKS });
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h2>My tasks</h2>
+//       <CategoryFilter />
+//       <NewTaskForm />
+//       <TaskList />
+//     </div>
+//   );
+// }
+
+// export default App;
+import React, { useState } from "react";
+import NewTaskForm from "./NewTaskForm";
+import CategoryFilter from "./CategoryFilter";
+import TaskList from "./TaskList";
+import { CATEGORIES } from "../data";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    { id: 1, text: "Buy rice", category: "Food" },
+    { id: 2, text: "Build a todo app", category: "Code" },
+    // Add more tasks as needed
+  ]);
+
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  const filteredTasks =
+    selectedCategory === "All"
+      ? tasks
+      : tasks.filter((task) => task.category === selectedCategory);
+
+  const handleTaskFormSubmit = (newTask) => {
+    setTasks([...tasks, { id: tasks.length + 1, ...newTask }]);
+  };
+
   return (
-    <div className="App">
-      <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+    <div>
+      <NewTaskForm categories={CATEGORIES} onTaskFormSubmit={handleTaskFormSubmit} />
+      <CategoryFilter
+        categories={CATEGORIES}
+        selectedCategory={selectedCategory}
+        onSelectCategory={setSelectedCategory}
+      />
+      <TaskList tasks={filteredTasks} />
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,25 @@
-import React from "react";
+// import React from "react";
 
-function TaskList() {
+// function TaskList() {
+//   return (
+//     <div className="tasks">
+//       {/* display a list of tasks using Task component */}
+//     </div>
+//   );
+// }
+
+// export default TaskList;
+import React from "react";
+import Task from "./Task";
+
+function TaskList({ tasks }) {
   return (
-    <div className="tasks">
-      {/* display a list of tasks using Task component */}
+    <div className="task-list">
+      {tasks.map((task) => (
+        <div key={task.id} className="task">
+          <Task text={task.text} category={task.category} />
+        </div>
+      ))}
     </div>
   );
 }
